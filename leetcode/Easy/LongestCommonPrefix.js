bf3a2d5fb21f6c2435f5e0a6332749fbd40c1d89
Karen <devkarenc@gmail.com>
2021-08-06 22:24:17 -0400

    Add Leetcode Easy - Longest Common Prefix

---
+++ leetcode/Easy/LongestCommonPrefix.js
@@ -0,0 +1,28 @@
+/*
+14. Longest Common Prefix
+
+Write a function to find the longest common prefix string amongst an array of strings.
+If there is no common prefix, return an empty string "".
+
+https://leetcode.com/problems/longest-common-prefix/
+*/
+
+// Solution with comments
+var longestCommonPrefix = function (strs) {
+  // take the first element in the strs array to compare across all elements
+  // since "common" means the prefix should exist in all elements
+  const firstWord = strs[0];
+  // for each character in the first word, compare with the same indexed character in each word in the strs array
+  for (let i = 0; i < firstWord.length; i++) {
+    const char = firstWord[i];
+    for (let j = 0; j < strs.length; j++) {
+      const word = strs[j];
+      // if the characters at the same index do not match,
+      // it's not common anymore, so there is no need to loop anymore
+      // simply return up until the characters were common
+      if (char !== word[i]) return firstWord.slice(0, i);
+    }
+  }
+  // if it never entered the if condition, it means the entire firstWord was common across all elements, so just return the firstWord as the longest common prefix.
+  return firstWord;
+};
--- leetcode/leetcode.md
+++ leetcode/leetcode.md
@@ -5,3 +5,4 @@
 1. [Reverse Integer](./Easy/ReverseInteger.js)
 2. [Palindrome Number](./Easy/PalindromeNumber.js)
 3. [Roman Numeral](./Easy/RomanToInteger.js)
+4. [Longest Common Prefix](./Easy/LongestCommonPrefix.js)
